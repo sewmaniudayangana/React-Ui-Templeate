b7e709dabdc8d963783ede55a08d8eb02495501a
sewmanudayangana <sewmaniudayangana@gmail.com>
2022-08-29 13:42:02 +0530

    Update Ui

---
+++ src/page/ProductManage/ProductManage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ProductManageForm.css'
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+const ProductManageForm = () => {
+  return (
+    <div>ProductManageForm</div>
+  )
+}
+
+export default ProductManageForm
