a055a33f1070588ebc5aa42b995ace80867e9e8e
sewmanudayangana <sewmaniudayangana@gmail.com>
2022-08-29 13:48:31 +0530

    Update Product

--- src/page/ProductManage/ProductManage.jsx
+++ src/page/ProductManage/ProductManage.jsx
@@ -8,8 +8,57 @@ import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
 const ProductManageForm = () => {
-  return (
-    <div>ProductManageForm</div>
+ 
+ 
+ const topitems = [
+    {label:'food'},
+    {label:'vegetable'},
+ ];
+ 
+ 
+ 
+ 
+    return (
+    <div>
+        <Typography variant="h5" gutterBottom style={{fontSize:'38px' , fontfamily:'comfortea,sans-serif'}} component="div">
+        ProductManage
+        </Typography>
+    
+        <div>
+         <TextField id="basic-field" label="Title" variant= "filled" style={{width:'56vh'}} className='text1' />
+         <TextField id="basic-field" label="Title" variant= "filled" style={{width:'56vh'}} className='text2' />
+        </div>
+    
+      
+        <div>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={top100Films}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Movie" />}
+          />
+        </div>
+
+        <div>
+        <TextareaAutosize
+          disablePortal
+          id="combo-box-demo"
+          placeholder='Description'
+          style={{ width: '56vh', height:'19vh'}}
+          className='textarea'
+          />
+        </div>
+       
+       <div>
+       <Button variant="contained" style={{width:'14vh', left:'140vh' , top:'229px'}} className='buttonone' color='primary'>Save</Button>
+       <Button variant="contained" style={{width:'14vh', top:'229px'}} className='buttontwo'>Cancel</Button>
+       </div>
+    
+    
+    </div>
+
+   
   )
 }
 
