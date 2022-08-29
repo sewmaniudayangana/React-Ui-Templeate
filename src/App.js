197c52c56537096cf287c3a70fed9cbd6415909c
sewmanudayangana <sewmaniudayangana@gmail.com>
2022-08-29 12:21:23 +0530

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {Route, Routes} from "react-router-dom";
 import Loginform from "./page/login/login";
+import Dashboardfrom from "./page/Dashboard/Dashboard";
 
 function App() {
   return (
    <div className="App">
    <Routes>
    <Route path='/page' element={<Loginform/>}/>
+   <Route path='/dashboard' element={<Dashboardfrom/>}/>
+   
    </Routes>
       
    </div>
---
+++ src/page/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Dashboard.css"
+
+function Dashboardfrom() {
+  return (
+    <div>Dashboardfrom</div>
+  )
+}
+
+export default Dashboardfrom
